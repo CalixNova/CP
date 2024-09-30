3e5b82e79bfb2599bf2950a60bf74b75d66e130e
Susmita Sanyal <susworld@mac.mynetworksettings.com>
2024-09-29 22:31:28 -0400

    wrote a class

--- algo-ds/main.cpp
+++ algo-ds/main.cpp
@@ -1,16 +1,53 @@
 #include <iostream>
 using namespace std;
 
+class Ractangle{
+private:
+    int length;
+    int breadth;
+
+public:
+    Ractangle(){
+        length =0;
+        breadth=0;
+    
+    }
+    Ractangle (int l, int b){
+        length = l;
+        breadth = b;
+    }
+    
+    int area(){
+        return length*breadth;
+    }
+    int perimeter(){
+        return 2*(length+breadth);
+    }
+    void setLength(int l){
+        length=l;
+    }
+    void setBreadth(int b){
+        breadth = b;
+    }
+    int getLength(){
+        return length;
+    }
+    int getBreadth(){
+        return breadth;
+    }
+    ~Ractangle(){
+        cout<<"done destructing";
+    }
+};
 
 int main(){
+
+    Ractangle r(10,5);
+    
+
+    cout<< "Area" << r.area()<< endl;
+    cout<< " Perimeter "<< r.perimeter() << endl;
    
-    int array[5];
-
-    array[0]=12;
-    array[1]=2;
-
-    cout<<sizeof(array);
-
     return 0;
 }
 
